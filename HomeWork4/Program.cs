370edd2c0857f29c5cdae9a134e3ac9c113211cb
olga <olgaviv@mail.ru>
2022-11-09 11:36:00 +0700

    second work

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-
+/*
 
 int Stepen (int a, int b)
 {
@@ -15,3 +15,32 @@ Console.Write("Введите степень числа: " );
 int b  = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine (Stepen (a,b));
+*/
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+int FindSum (int number)
+{
+  int sum = number % 10;
+ 
+
+ 
+     while (number >= 10)
+    {
+      number = number/10;
+        
+       sum = sum + (number % 10); 
+    } 
+ 
+
+    return sum;
+}
+
+Console.Write("Input a number:" );
+int a  = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine ($" Sum of number {a} is {FindSum(a)}");
+
